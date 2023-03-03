8c64e642c9e3c8432ee57ce0db8a5e4c26e3172d
PdskDev <prestadesk@gmail.com>
2023-03-03 20:11:35 +0100

    Add Current Weather component + Interface

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+
+import { ICurrentWeather } from "../Model/interfaces";
+
+@Component({
+  selector: "app-current-weather",
+  templateUrl: "./current-weather.component.html",
+  styleUrls: ["./current-weather.component.css"],
+})
+export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather;
+
+  constructor() {
+    this.current = {
+      city: "Brazzaville",
+      country: "CG",
+      date: new Date(),
+      image: "assets/img/sunny.svg",
+      temperature: 72,
+      description: "sunny",
+    } as ICurrentWeather;
+  }
+
+  ngOnInit() {}
+}
