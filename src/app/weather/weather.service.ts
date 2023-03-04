80361a463bec4be113d755cbbef9cd927f2e2577
PdskDev <prestadesk@gmail.com>
2023-03-04 14:50:40 +0100

    Refactoring Api Uri in Service

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,4 +1,5 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
+
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
 
@@ -26,9 +27,18 @@ export class WeatherService {
   constructor(private httpClient: HttpClient) {}
 
   getCurrentWeather(city: string, country: string) {
-    return this.httpClient.get<ICurrentWeatherData>(
+    const uriParams = new HttpParams()
+      .set("q", `${city},${country}`)
+      .set("appid", environment.appId);
+
+    /* return this.httpClient.get<ICurrentWeatherData>(
       `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?` +
         `q=${city},${country}&appid=${environment.appId}`
+    ); */
+
+    return this.httpClient.get<ICurrentWeatherData>(
+      `${environment.baseUrl}api.openweathermap.org/data/2.5/weather`,
+      { params: uriParams }
     );
   }
 }
